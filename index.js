dd2fcd397f7a84ead9522bc2e6b81b2a36d563fe
jonschlinkert <dev@sellside.com>
2014-11-15 22:40:57 -0500

    update load method to run a stack of loaders

--- index.js
+++ index.js
@@ -21,13 +21,14 @@ function Loaders() {
 }
 
 Loaders.prototype.register = function(ext, fn) {
-  this.cache[ext] = [fn];
+  this.cache[this.ext(ext)] = [fn];
   return this;
 };
 
 Loaders.prototype.compose = function(ext, loaders) {
   return loaders.reduce(function(stack, loader) {
-    (stack[ext] = stack[ext] || []).push(this.cache[loader]);
+    stack[ext] = stack[ext] || [];
+    stack[ext] = stack[ext].concat(this.cache[loader]);
     return stack;
   }.bind(this), this.cache);
 };
@@ -37,7 +38,19 @@ Loaders.prototype.read = function(fp, options) {
   return fn(path.resolve(fp), options);
 };
 
-Loaders.prototype.load = function(fp, options) {
-  var fn = this.cache[path.extname(fp)];
+Loaders.prototype._read = function(fn, fp, options) {
   return fn(path.resolve(fp), options);
+};
+
+Loaders.prototype.ext = function(ext) {
+  return ext[0] === '.' ? ext.slice(1) : ext;
+};
+
+Loaders.prototype.load = function(fp, options) {
+  var ext = path.extname(fp);
+  var fns = this.cache[ext.slice(1)];
+
+  return fns.reduce(function (acc, fn) {
+    return fn(acc, options);
+  }, fp);
 };
--- test.js
+++ test.js
@@ -7,6 +7,7 @@
 
 'use strict';
 
+var fs = require('fs');
 var should = require('should');
 var YAML = require('js-yaml');
 var Loaders = require('./');
@@ -15,15 +16,13 @@ var loaders;
 describe('loaders', function () {
   beforeEach(function() {
     loaders = new Loaders();
-  });
 
-  it('should register a loader:', function () {
-    loaders.register('yaml', function yaml(fp, options) {
-      return YAML.safeLoad(fp, options);
+    loaders.register('yaml', function yaml(fp) {
+      return YAML.safeLoad(fp);
     });
 
-    loaders.register('yml', function yml(fp, options) {
-      return YAML.safeLoad(fp, options);
+    loaders.register('yml', function yml(fp) {
+      return YAML.safeLoad(fp);
     });
 
     loaders.register('json', function json(fp) {
@@ -37,15 +36,19 @@ describe('loaders', function () {
     loaders.register('hbs', function hbs(fp) {
       return fs.readFileSync(fp, 'utf8');
     });
-
-    loaders.register('hbs', function hbs(fp) {
-      return fs.readFileSync(fp, 'utf8');
-    });
-
-    loaders.compose('foo', ['txt', 'yaml']);
-
-    console.log(loaders.cache)
-    loaders.cache.should.have.properties('yaml', 'yml');
   });
 
+  it('should register loaders:', function () {
+    loaders.cache.should.have.properties('yaml', 'yml', 'json', 'txt', 'hbs');
+  });
+
+  it('should compose a loader from other loaders:', function () {
+    loaders.compose('foo', ['txt', 'yaml']);
+    loaders.cache.should.have.property('foo');
+  });
+
+  it('should run a loader stack on the given filepath:', function () {
+    loaders.compose('foo', ['txt', 'yaml']);
+    loaders.load('fixtures/a.foo').should.eql({a: 'b'});
+  });
 });
