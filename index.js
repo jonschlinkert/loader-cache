86fe3a506049056bff91ecac2d1ea743a4d29846
jonschlinkert <dev@sellside.com>
2014-11-15 23:37:16 -0500

    update compose, load, register methods

--- index.js
+++ index.js
@@ -15,15 +15,93 @@ var path = require('path');
 
 module.exports = Loaders;
 
+/**
+ * Create a new instance of `Loader`
+ *
+ * ```js
+ * var Loader = require('loader');
+ * var loader = new Loader();
+ * ```
+ *
+ * @class `Loader`
+ * @api public
+ */
 
 function Loaders() {
   this.cache = {};
 }
 
+/**
+ * Register the given loader callback `fn` as `ext`. Any arbitrary
+ * name can be assigned to a loader, however, the loader will only be
+ * called when either:
+ *   a. `ext` matches the file extension of a path passed to the `.load()` method, or
+ *   b. `ext` is an arbitrary name passed on the loader stack of another loader. Example below.
+ *
+ * **Examples**
+ *
+ * ```js
+ * // register a loader for parsing YAML
+ * loaders.register('yaml', function(fp) {
+ *   return YAML.safeLoad(fp);
+ * });
+ *
+ * // register a loader to be used in other loaders
+ * loaders.register('read', function(fp) {
+ *   return fs.readFileSync(fp, 'utf8');
+ * });
+ *
+ * // create a new loader from the `yaml` and `read` loaders.
+ * loaders.register('yml', ['read', 'yaml']);
+ * ```
+ *
+ * @param {String|Array} `ext` File extension or name of the loader.
+ * @param {Function|Array} `fn` A loader function, or create a loader from other others by passing an array of names.
+ * @return {Object} `Loaders` to enable chaining
+ * @api public
+ */
+
 Loaders.prototype.register = function(ext, fn) {
-  this.cache[this.ext(ext)] = [fn];
+  ext = (ext[0] === '.') ? ext.slice(1) : ext;
+
+  if (Array.isArray(fn)) {
+    return this.compose(ext, fn);
+  }
+
+  this.cache[ext] = [fn];
   return this;
 };
+
+/**
+ * Create a loader from other (previously cached) loaders. For
+ * example, you might create a loader like the following:
+ *
+ * **Example**
+ *
+ * ```js
+ * // arbitrary name, so it won't match file extensions. This
+ * // loader will be used in other loaders for reading files
+ * loaders.register('read', function(fp) {
+ *   return fs.readFileSync(fp, 'utf8');
+ * });
+ *
+ * // Parse a string of YAML
+ * loaders.register('yaml', function(fp) {
+ *   return YAML.safeLoad(fp);
+ * });
+ *
+ * // Compose a new loader that will read a file, then parse it as YAML
+ * loaders.compose('yml', ['read', 'yaml']);
+ *
+ * // you can alternatively do the same thing with the register method, e.g.
+ * loaders.register('yml', ['read', 'yaml']);
+ * ```
+ *
+ * @param {String} `ext` File extension to select the loader or loader stack to use.
+ * @param {String} `loaders` Array of loader names.
+ * @return {String}
+ * @api private
+ */
 
 Loaders.prototype.compose = function(ext, loaders) {
   return loaders.reduce(function(stack, loader) {
@@ -33,22 +111,26 @@ Loaders.prototype.compose = function(ext, loaders) {
   }.bind(this), this.cache);
 };
 
-Loaders.prototype.read = function(fp, options) {
-  var fn = this.cache[path.extname(fp)];
-  return fn(path.resolve(fp), options);
-};
-
-Loaders.prototype._read = function(fn, fp, options) {
-  return fn(path.resolve(fp), options);
-};
-
-Loaders.prototype.ext = function(ext) {
-  return ext[0] === '.' ? ext.slice(1) : ext;
-};
+/**
+ * Run loaders associated with `ext` of the given filepath.
+ *
+ * **Example**
+ *
+ * ```js
+ * // this will run the `yml` loader from the `.compose()` example
+ * loaders.load('config.yml');
+ * ```
+ *
+ * @param {String} `fp` File path to load.
+ * @param {String} `options` Options to pass to whatever loaders are defined.
+ * @return {String}
+ * @api public
+ */
 
 Loaders.prototype.load = function(fp, options) {
   var ext = path.extname(fp);
   var fns = this.cache[ext.slice(1)];
+  if (!fns) return fp;
 
   return fns.reduce(function (acc, fn) {
     return fn(acc, options);
