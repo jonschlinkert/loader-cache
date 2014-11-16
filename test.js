9006b4487e79576d46fa711c8a064b52f2ed5909
jonschlinkert <dev@sellside.com>
2014-11-15 23:35:58 -0500

    adds test, fixtures

--- test.js
+++ test.js
@@ -29,26 +29,48 @@ describe('loaders', function () {
       return require(path.resolve(fp));
     });
 
-    loaders.register('txt', function txt(fp) {
+    loaders.register('read', function read(fp) {
       return fs.readFileSync(fp, 'utf8');
     });
 
     loaders.register('hbs', function hbs(fp) {
       return fs.readFileSync(fp, 'utf8');
     });
+
+    loaders.register('data', function data(obj) {
+      obj.e = 'f';
+      return obj;
+    });
   });
 
   it('should register loaders:', function () {
-    loaders.cache.should.have.properties('yaml', 'yml', 'json', 'txt', 'hbs');
+    loaders.cache.should.have.properties('yaml', 'yml', 'json', 'read', 'hbs');
   });
 
-  it('should compose a loader from other loaders:', function () {
-    loaders.compose('foo', ['txt', 'yaml']);
+  it('should compose a loader from other loaders with the `.compose()` method:', function () {
+    loaders.compose('foo', ['read', 'yaml']);
     loaders.cache.should.have.property('foo');
   });
 
-  it('should run a loader stack on the given filepath:', function () {
-    loaders.compose('foo', ['txt', 'yaml']);
-    loaders.load('fixtures/a.foo').should.eql({a: 'b'});
+  it('should compose a loader from other loaders with the `.register()` method:', function () {
+    loaders.register('foo', ['read', 'yaml']);
+    loaders.cache.should.have.property('foo');
+  });
+
+  it('should pass the value returned from a loader to the next loader:', function () {
+    loaders.register('bar', ['read', 'yaml', 'data']);
+    loaders.load('fixtures/a.bar').should.eql({c: 'd', e: 'f'});
+  });
+
+  it('should pass the value returned from a loader to the next loader:', function () {
+    loaders.compose('bar', ['read', 'yaml', 'data']);
+    loaders.load('fixtures/a.bar').should.eql({c: 'd', e: 'f'});
+  });
+
+  it('should compose a loader from other loaders:', function () {
+    loaders.compose('parse', ['read', 'yaml']);
+    loaders.compose('extend', ['data']);
+    loaders.compose('bar', ['parse', 'extend']);
+    loaders.load('fixtures/a.bar').should.eql({c: 'd', e: 'f'});
   });
 });
